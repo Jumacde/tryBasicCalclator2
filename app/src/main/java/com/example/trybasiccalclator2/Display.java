abd3a0952d7994543f04af919db720e666861240
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-06-10 20:56:55 +0200

    commit

--- app/src/main/java/com/example/trybasiccalclator2/Display.java
+++ app/src/main/java/com/example/trybasiccalclator2/Display.java
@@ -2,5 +2,8 @@ package com.example.trybasiccalclator2;
 
 public interface Display {
     String getDisplay();
+    String getCalcStep();
     void setDisplay();
+    void setCalcStep();
+
 }
--- app/src/main/java/com/example/trybasiccalclator2/impl/Display_impl.java
+++ app/src/main/java/com/example/trybasiccalclator2/impl/Display_impl.java
@@ -2,6 +2,7 @@ package com.example.trybasiccalclator2.impl;
 
 public class Display_impl {
     private String display;
+    private String calcStep;
 
     public Display_impl() {}
 }
