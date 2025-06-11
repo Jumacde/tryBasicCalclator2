12a1400d01eb74c5b696982b912693fb19fcc709
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-06-11 19:25:17 +0200

    Commit

--- app/src/main/java/com/example/trybasiccalclator2/MainActivity.java
+++ app/src/main/java/com/example/trybasiccalclator2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.trybasiccalclator2;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.trybasiccalclator2.impl.Calc_impl;
+import com.example.trybasiccalclator2.impl.ClickFunction_impl;
+import com.example.trybasiccalclator2.impl.Display_impl;
+import com.example.trybasiccalclator2.impl.Operator_impl;
+
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private Button pl, mi, mu, di, eq;
+
+    private Calc calc;
+    private Operator operator;
+    private Display display;
+    private ClickFunction clickFunction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        calc = new Calc_impl();
+        operator = new Operator_impl();
+        display = new Display_impl();
+        clickFunction = new ClickFunction_impl();
+
     }
 }
--- app/src/main/java/com/example/trybasiccalclator2/impl/Operator_impl.java
+++ app/src/main/java/com/example/trybasiccalclator2/impl/Operator_impl.java
@@ -1,8 +1,30 @@
 package com.example.trybasiccalclator2.impl;
 
-public class Operator_impl {
+import com.example.trybasiccalclator2.Operator;
+
+public class Operator_impl implements Operator {
     private String operator;
     private String sOpe; // stored operator
 
     public Operator_impl() {}
+
+    @Override
+    public String getOperator() {
+        return operator;
+    }
+
+    @Override
+    public String getSOpe() {
+        return sOpe;
+    }
+
+    @Override
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    @Override
+    public void setSOpe(String sOpe) {
+        this.sOpe = sOpe;
+    }
 }
