d6a5ac89258e91449a3b95b33e5fa2b5590e7358
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-06-11 13:13:36 +0200

    commit

--- app/src/main/java/com/example/trybasiccalclator2/Display.java
+++ app/src/main/java/com/example/trybasiccalclator2/Display.java
@@ -2,9 +2,7 @@ package com.example.trybasiccalclator2;
 
 public interface Display {
     String getDisplay();
-    String getCalcStep();
     void setDisplay();
-    void setCalcStep();
 
     void callShowCalcStep(); // wrap method
 
--- app/src/main/java/com/example/trybasiccalclator2/impl/Display_impl.java
+++ app/src/main/java/com/example/trybasiccalclator2/impl/Display_impl.java
@@ -4,11 +4,9 @@ import com.example.trybasiccalclator2.Display;
 
 public class Display_impl implements Display {
     private String display;
-    private String calcStep;
 
     public Display_impl() {
         this.display = "0";
-        this.calcStep = "0";
     }
 
     @Override
@@ -17,29 +15,14 @@ public class Display_impl implements Display {
     }
 
     @Override
-    public String getCalcStep() {
-        return calcStep;
-    }
-
-    @Override
     public void setDisplay() {
         this.display = display;
-    }
-
-    @Override
-    public void setCalcStep() {
-        this.calcStep = calcStep;
     }
 
     // wrap method
     @Override
     public void callShowCalcStep() {
 
-    }
-
-    private String showCalcStep() {
-
-        return calcStep;
     }
 
 }
