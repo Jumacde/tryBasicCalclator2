03a12dc73abd892276ea5da512718549df87781d
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-06-11 19:30:18 +0200

    Commit

--- app/src/main/java/com/example/trybasiccalclator2/MainActivity.java
+++ app/src/main/java/com/example/trybasiccalclator2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.trybasiccalclator2;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -35,5 +36,87 @@ public class MainActivity extends AppCompatActivity {
         display = new Display_impl();
         clickFunction = new ClickFunction_impl();
 
+        textView = findViewById(R.id.text);
+        pl = findViewById(R.id.plass);
+        mi = findViewById(R.id.min);
+        mu = findViewById(R.id.mul);
+        di = findViewById(R.id.div);
+        eq = findViewById(R.id.equal);
+
+        updateDisplay();
+
+        pl.setOnClickListener(v -> clickFunction.callOperatorClick(calc, display, operator, "+"));
+        mi.setOnClickListener(v -> clickFunction.callOperatorClick(calc, display, operator, "-"));
+        mu.setOnClickListener(v -> clickFunction.callOperatorClick(calc, display, operator, "*"));
+        di.setOnClickListener(v -> clickFunction.callOperatorClick(calc, display, operator, "/"));
+        eq.setOnClickListener(v -> clickFunction.callOperatorClick(calc, display, operator, "="));
+
+
     }
+
+    public void press00(View view) {
+        clickFunction.callDigitClick(calc, display, operator, 0);
+        clickFunction.callDigitClick(calc, display, operator, 0);
+        updateDisplay();
+    }
+
+    public void press0(View view) {
+        clickFunction.callDigitClick(calc, display, operator, 0);
+        updateDisplay();
+    }
+
+    public void press1(View view) {
+        clickFunction.callDigitClick(calc, display, operator, 1);
+        updateDisplay();
+    }
+
+    public void press2(View view) {
+        clickFunction.callDigitClick(calc, display, operator, 2);
+        updateDisplay();
+    }
+
+    public void press3(View view) {
+        clickFunction.callDigitClick(calc, display, operator, 3);
+        updateDisplay();
+    }
+
+    public void press4(View view) {
+        clickFunction.callDigitClick(calc, display, operator, 4);
+        updateDisplay();
+    }
+
+    public void press5(View view) {
+        clickFunction.callDigitClick(calc, display, operator, 5);
+        updateDisplay();
+    }
+
+    public void press6(View view) {
+        clickFunction.callDigitClick(calc, display, operator, 6);
+        updateDisplay();
+    }
+
+    public void press7(View view) {
+        clickFunction.callDigitClick(calc, display, operator, 7);
+        updateDisplay();
+    }
+
+    public void press8(View view) {
+        clickFunction.callDigitClick(calc, display, operator, 8);
+        updateDisplay();
+    }
+
+    public void press9(View view) {
+        clickFunction.callDigitClick(calc, display, operator, 9);
+        updateDisplay();
+    }
+
+    public void pressAc(View view) {
+        clickFunction.callACClick(calc, display, operator);
+        updateDisplay();
+    }
+
+    public void updateDisplay() {
+        textView.setText(display.getDisplay());
+    }
+
 }
