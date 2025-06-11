21f4a6f20d8ed829ced9f7d8593e6966a1b45537
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-06-11 19:41:59 +0200

    Commit

--- app/src/main/java/com/example/trybasiccalclator2/impl/ClickFunction_impl.java
+++ app/src/main/java/com/example/trybasiccalclator2/impl/ClickFunction_impl.java
@@ -34,6 +34,11 @@ public class ClickFunction_impl implements ClickFunction {
     }
 
     private void operatorClick(Calc calc, Operator operator, Display display) {
+        String op = operator.getSOpe();
+        if (op.equals("=")) {
+            String x;
+            x =
+        }
 
     }
 
