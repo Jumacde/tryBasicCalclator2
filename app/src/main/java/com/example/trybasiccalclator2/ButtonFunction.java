b85a28697585cbe3e8e21071b1d713a24b4c7c2a
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-06-13 21:29:58 +0200

    Commit

--- app/src/main/java/com/example/trybasiccalclator2/ButtonFunction.java
+++ app/src/main/java/com/example/trybasiccalclator2/ButtonFunction.java
@@ -1,4 +1,8 @@
 package com.example.trybasiccalclator2;
 
 public interface ButtonFunction {
+    void callOnNumberClick(CalculateLogic calculateLogic);
+    void callOnOperatorCLick(Operators operators);
+    void callOnEqualClick(Operators operators);
+    void callOnACClick(TextDisplay textDisplay);
 }
--- app/src/main/java/com/example/trybasiccalclator2/impl/ButtonFunction_impl.java
+++ app/src/main/java/com/example/trybasiccalclator2/impl/ButtonFunction_impl.java
@@ -1,6 +1,9 @@
 package com.example.trybasiccalclator2.impl;
 
 import com.example.trybasiccalclator2.ButtonFunction;
+import com.example.trybasiccalclator2.CalculateLogic;
+import com.example.trybasiccalclator2.Operators;
+import com.example.trybasiccalclator2.TextDisplay;
 
 public class ButtonFunction_impl implements ButtonFunction {
 
@@ -8,4 +11,43 @@ public class ButtonFunction_impl implements ButtonFunction {
 
     }
 
+    /**
+     * wrap method
+     * @ Param: calculateLogic
+     * - call the interface
+     * */
+    @Override
+    public void callOnNumberClick(CalculateLogic calculateLogic) {
+
+    }
+
+    /**
+     * wrap method
+     * @ Param: operators
+     * - call the interface
+     * */
+    @Override
+    public void callOnOperatorCLick(Operators operators) {
+
+    }
+
+    /**
+     * wrap method
+     * @ Param: operators
+     * - call the interface
+     * */
+    @Override
+    public void callOnEqualClick(Operators operators) {
+
+    }
+
+    /**
+     * wrap method
+     * @ Param: textDisplay
+     * - call the interface
+     * */
+    @Override
+    public void callOnACClick(TextDisplay textDisplay) {
+
+    }
 }
