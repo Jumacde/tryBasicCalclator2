4447d5b953b9572341af8dacb6b32052edf34051
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-06-16 21:20:16 +0200

    Commit

--- app/src/main/java/com/example/trybasiccalclator2/MainActivity.java
+++ app/src/main/java/com/example/trybasiccalclator2/MainActivity.java
@@ -7,10 +7,18 @@ import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.trybasiccalclator2.impl.ButtonFunction_impl;
+import com.example.trybasiccalclator2.impl.CalculateLogic_impl;
+import com.example.trybasiccalclator2.impl.Operators_impl;
+import com.example.trybasiccalclator2.impl.TextDisplay_impl;
+
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private Button pl, mi, mu, di, eq;
-
+    private CalculateLogic calculateLogic;
+    private Operators operators;
+    private TextDisplay textDisplay;
+    private ButtonFunction buttonFunction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        calculateLogic = new CalculateLogic_impl();
+        operators = new Operators_impl();
+        textDisplay = new TextDisplay_impl(calculateLogic, operators);
+        buttonFunction = new ButtonFunction_impl(calculateLogic, operators, textDisplay);
 
 
     }
